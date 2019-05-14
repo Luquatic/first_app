651a6430482a7adf3c3cd38ec35c57bbb1194731
Luquatic <jesseyfransen@live.nl>
2019-05-14 22:38:54 +0200

    splitted code

--- lib/products.dart
+++ lib/products.dart
@@ -2,21 +2,23 @@ import 'package:flutter/material.dart';
 
 class Products extends StatelessWidget {
   final List<String> products;
-  
-  Products(this.products)
+
+  Products(this.products);
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: products
-          .map((element) => Card(
-                child: Column(
-                  children: <Widget>[
-                    Image.asset('assets/food.jpg'),
-                    Text(element)
-                  ],
+          .map(
+            (element) => Card(
+                  child: Column(
+                    children: <Widget>[
+                      Image.asset('assets/food.jpg'),
+                      Text(element)
+                    ],
+                  ),
                 ),
-              ))
+          )
           .toList(),
     );
   }
