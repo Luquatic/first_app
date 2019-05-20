54f3e88e6b65a210c19516b08f43c6c647eb7409
Luquatic <jesseyfransen@live.nl>
2019-05-20 19:38:37 +0200

    added deleteProduct function

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -1,20 +1,33 @@
 import 'package:flutter/material.dart';
 
 class ProductPage extends StatelessWidget {
+  final String title;
+  final String imageUrl;
+
+  ProductPage(this.title, this.imageUrl);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Product details'),
+        title: Text(title),
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          Image.asset('assets/food.jpg'),
+          Image.asset(imageUrl),
           Container(
             padding: EdgeInsets.all(10.0),
-            child: Text('On the Product Page'),
+            child: Text(title),
           ),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: RaisedButton(
+              color: Theme.of(context).buttonColor,
+              child: Text('DELETE'),
+              onPressed: () => Navigator.pop(context, true),
+            ),
+          )
         ],
       ),
     );
