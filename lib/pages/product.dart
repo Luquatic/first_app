5c569d8bef65785ada40a0f5d83c9dd98447f7b2
Luquatic <jesseyfransen@live.nl>
2019-05-20 19:06:38 +0200

    added product page

---
+++ lib/pages/product.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ProductPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Product details'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Image.asset('assets/food.jpg'),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Text('On the Product Page'),
+          ),
+        ],
+      ),
+    );
+  }
+}
