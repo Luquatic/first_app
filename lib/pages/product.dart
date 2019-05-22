cca193afe288b46b27c2ddb538d269f5c0c7a9bc
Luquatic <jesseyfransen@live.nl>
2019-05-22 16:32:51 +0200

    added delete dialog

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -7,6 +7,31 @@ class ProductPage extends StatelessWidget {
 
   ProductPage(this.title, this.imageUrl);
 
+  _showWarningDialog(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Are you sure?'),
+            content: Text('This cannot be undone'),
+            actions: <Widget>[
+              FlatButton(
+                  child: Text('DISCARD'),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  }),
+              FlatButton(
+                child: Text('CONFIRM'),
+                onPressed: () {
+                  Navigator.pop(context);
+                  Navigator.pop(context, true);
+                },
+              ),
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
@@ -31,7 +56,7 @@ class ProductPage extends StatelessWidget {
               child: RaisedButton(
                 color: Theme.of(context).buttonColor,
                 child: Text('DELETE'),
-                onPressed: () => Navigator.pop(context, true),
+                onPressed: () => _showWarningDialog(context),
               ),
             )
           ],
