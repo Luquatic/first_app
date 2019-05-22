a99d335e2d1016ea9a08311843c974e15daaee23
Luquatic <jesseyfransen@live.nl>
2019-05-22 19:52:47 +0200

    refactoring

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -1,10 +1,67 @@
 import 'package:flutter/material.dart';
 
-class ProductCreatePage extends StatelessWidget {
+class ProductCreatePage extends StatefulWidget {
+  final Function addProduct;
+
+  ProductCreatePage(this.addProduct);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ProductCreatePageState();
+  }
+}
+
+class _ProductCreatePageState extends State<ProductCreatePage> {
+  String titleValue;
+  String descriptionValue;
+  double priceValue;
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Create Product'),
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      child: ListView(
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(labelText: 'Title'),
+            onChanged: (String value) {
+              setState(() {
+                titleValue = value;
+              });
+            },
+          ),
+          TextField(
+            maxLines: 4,
+            decoration: InputDecoration(labelText: 'Description'),
+            onChanged: (String value) {
+              setState(() {
+                descriptionValue = value;
+              });
+            },
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(labelText: 'Price'),
+            onChanged: (String value) {
+              setState(() {
+                priceValue = double.parse(value);
+              });
+            },
+          ),
+          RaisedButton(
+            child: Text('Save'),
+            onPressed: () {
+              final Map<String, dynamic> product = {
+                'title': titleValue,
+                'description': descriptionValue,
+                'price': priceValue,
+                'image': 'assets/food.jpg'
+              };
+              widget.addProduct(product);
+            },
+          )
+        ],
+      ),
     );
   }
 }
--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 
 import './products.dart';
-import './product_control.dart';
 
 class ProductManager extends StatelessWidget {
-  final List<Map<String, String>> products;
-  final Function addProduct;
-  final Function deleteProduct;
+  final List<Map<String, dynamic>> products;
 
-  ProductManager(this.products, this.addProduct, this.deleteProduct);
+  ProductManager(this.products);
 
   @override
   Widget build(BuildContext context) {
@@ -16,11 +13,7 @@ class ProductManager extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.end,
       children: [
-        Expanded(child: Products(products, deleteProduct: deleteProduct)),
-        Container(
-          margin: EdgeInsets.all(10.0),
-          child: ProductControl(addProduct),
-        ),
+        Expanded(child: Products(products)),
       ],
     );
   }
