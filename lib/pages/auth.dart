059cc2daa884d9a4efd05732e918e84b05bbb1f9
Luquatic <jesseyfransen@live.nl>
2019-05-22 21:23:18 +0200

    added login

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 
-import './products.dart';
+class AuthPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _AuthPageState();
+  }
+}
 
-class AuthPage extends StatelessWidget {
+class _AuthPageState extends State<AuthPage> {
+  String _usernameValue;
+  String _passwordValue;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,11 +18,46 @@ class AuthPage extends StatelessWidget {
         title: Text('Login'),
       ),
       body: Center(
-        child: RaisedButton(
-          child: Text('LOGIN'),
-          onPressed: () {
-            Navigator.pushReplacementNamed(context, '/');
-          },
+        child: Container(
+          width: MediaQuery.of(context).size.width * 0.80,
+          child: ListView(shrinkWrap: true, children: <Widget>[
+            TextField(
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Username',
+              ),
+              onChanged: (String value) {
+                setState(() {
+                  _usernameValue = value;
+                });
+              },
+            ),
+            SizedBox(height: 10.0),
+            TextField(
+              textAlign: TextAlign.center,
+              obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Password',
+              ),
+              onChanged: (String value) {
+                setState(() {
+                  _passwordValue = value;
+                });
+              },
+            ),
+            RaisedButton(
+              color: Theme.of(context).primaryColor,
+              textColor: Colors.white,
+              child: Text('LOGIN'),
+              onPressed: () {
+                print(_usernameValue);
+                print(_passwordValue);
+                Navigator.pushReplacementNamed(context, '/products');
+              },
+            ),
+          ]),
         ),
       ),
     );
