59daa75f3fc17a27afc198455209e9223b3da945
Luquatic <jesseyfransen@live.nl>
2019-05-27 20:02:20 +0200

    refactored product_create

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -16,52 +16,70 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
   String _descriptionValue;
   double _priceValue;
 
+  Widget _buildTitleTextField() {
+    return TextField(
+      decoration: InputDecoration(labelText: 'Title'),
+      onChanged: (String value) {
+        setState(() {
+          _titleValue = value;
+        });
+      },
+    );
+  }
+
+  Widget _buildDescriptionTextField() {
+    return TextField(
+      maxLines: 4,
+      decoration: InputDecoration(labelText: 'Description'),
+      onChanged: (String value) {
+        setState(() {
+          _descriptionValue = value;
+        });
+      },
+    );
+  }
+
+  Widget _buildPriceTextField() {
+    return TextField(
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(labelText: 'Price'),
+      onChanged: (String value) {
+        setState(() {
+          _priceValue = double.parse(value);
+        });
+      },
+    );
+  }
+
+  Widget _buildRaisedButton() {
+    return RaisedButton(
+      child: Text('Save'),
+      onPressed: () {
+        final Map<String, dynamic> product = {
+          'title': _titleValue,
+          'description': _descriptionValue,
+          'price': _priceValue,
+          'image': 'assets/images/food.jpg'
+        };
+        widget.addProduct(product);
+        Navigator.pushReplacementNamed(context, '/products');
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(15.0),
       child: ListView(
         children: <Widget>[
-          TextField(
-            decoration: InputDecoration(labelText: 'Title'),
-            onChanged: (String value) {
-              setState(() {
-                _titleValue = value;
-              });
-            },
+          _buildTitleTextField(),
+          _buildDescriptionTextField(),
+          _buildPriceTextField(),
+          SizedBox(
+            height: 10.0,
           ),
-          TextField(
-            maxLines: 4,
-            decoration: InputDecoration(labelText: 'Description'),
-            onChanged: (String value) {
-              setState(() {
-                _descriptionValue = value;
-              });
-            },
-          ),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(labelText: 'Price'),
-            onChanged: (String value) {
-              setState(() {
-                _priceValue = double.parse(value);
-              });
-            },
-          ),
-          SizedBox(height: 10.0,),
-          RaisedButton(
-            child: Text('Save'),
-            onPressed: () {
-              final Map<String, dynamic> product = {
-                'title': _titleValue,
-                'description': _descriptionValue,
-                'price': _priceValue,
-                'image': 'assets/images/food.jpg'
-              };
-              widget.addProduct(product);
-              Navigator.pushReplacementNamed(context, '/products');
-            },
-          )
+          _buildRaisedButton(),
         ],
       ),
     );
